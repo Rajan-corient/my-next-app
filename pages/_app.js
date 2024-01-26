3573cb85c0a49e6e432626052bdeb91013c3187e
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2024-01-26 14:40:25 +0530

    routing added in blog app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <div>Rajan Gupta</div>;
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
