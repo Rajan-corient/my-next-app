9e15428dc2ccc7213300d1d7da9ae4687e0e2f0a
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2024-01-27 17:03:23 +0530

    blogs shown using api

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,15 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/
+// http://localhost:3000/api/blog
 const fs = require("node:fs");
-export default function handler(req, res) {
-  let fileData = "";
-  fs.readdir("blogdata", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(typeof data);
-    console.log(data);
-    res.status(200).json(data);
-  });
-  //   res.status(200).json({ name: "Hello from Rajan Gupta!" });
+
+export default async function handler(req, res) {
+  let allBlogs = [];
+  //   fs.readdir("blogdata", "utf8", (err, data) => {
+  //     if (err) {
+  //       console.error(err);
+  //       return;
+  //     }
+  //     data.forEach((item) => {
+  //       console.log(item);
+  //       allBlogs.push(item);
+  //     });
+  //     res.status(200).json(allBlogs);
+  //   });
+
+  const data = await fs.promises.readdir("blogdata");
+  for (const item of data) {
+    const filedata = await fs.promises.readFile(`blogdata/${item}`, "utf-8");
+    allBlogs.push(JSON.parse(filedata));
+  }
+  console.log(data);
+  res.status(200).json(allBlogs);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/Link";
 import styles from "../styles/blog.module.css";
 
 // step1: Read all the files from blogdata directory
 // step2: Iterate through then and display in blog component
 const Blog = () => {
+  const [blogList, setBlogList] = useState([]);
+
+  useEffect(() => {
+    console.log("blog component rendered");
+    getBlogs();
+  }, []);
+
+  const getBlogs = async () => {
+    const apiUrl = "http://localhost:3000/api/blog";
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    setBlogList(data);
+    console.log("data", data);
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <Link href={"/blogpost/learn-next"}>
-          <div className={styles.blogItem}>
-            <h3>How to learn Next in 2024</h3>
-            <p>Next js in a react framework</p>
+        {blogList.map((item) => (
+          <div className={styles.blogItem} key={item.slug}>
+            <h3>{item.title}</h3>
+            <p>
+              {item.content.substr(0, 50)}.....
+              <Link href={`/blogpost/${item.slug}`}>
+                <span className={styles.readMore}>Read more</span>
+              </Link>
+            </p>
+            <p>{item.author}</p>
           </div>
-        </Link>
-        <div className={styles.blogItem}>
-          <h3>How to learn Next in 2024</h3>
-          <p>Next js in a react framework</p>
-        </div>
-        <div className={styles.blogItem}>
-          <h3>How to learn Next in 2024</h3>
-          <p>Next js in a react framework</p>
-        </div>
+        ))}
       </main>
     </div>
   );
